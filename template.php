144160b7baa9f6df763ac54f426b30afeccfa98d
Rob Curtis <surreymagpie@yahoo.co.uk>
2015-06-30 12:39:39 +0100

    Add page template and viewport tag

--- template.php
+++ template.php
@@ -6,4 +6,14 @@ function practice_preprocess_html(&$variables) {
 
   // Import Google font 'Roboto'
   drupal_add_css('http://fonts.googleapis.com/css?family=Roboto:100,300,400,700');
+
+  // Add a viewport meta tag to the <head> for mobile responsiveness
+  $element = array(
+    '#tag' => 'meta', // The #tag is the html tag - <meta />
+    '#attributes' => array( // Set up an array of attributes inside the tag
+      'name' => 'viewport',
+      'content' => 'width=device-width, initial-scale=1',
+    ),
+  );
+  drupal_add_html_head($element, 'viewport_meta_tag');
 }
