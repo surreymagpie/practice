cd519743d41fed7679e852a328bbe6ac32fe07c9
Rob Curtis <surreymagpie@yahoo.co.uk>
2015-07-02 11:52:25 +0100

    Add default logo and wrap title in link

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -84,14 +84,16 @@
       <div class="site__masthead">
 
         <?php if ($logo): ?>
-          <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo">
+          <a href="<?php print $front_page; ?>" title="<?php print t('Go to home page'); ?>" rel="home" id="logo">
             <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
           </a>
         <?php endif; ?>
 
         <div class="site__branding">
           <?php if ($site_name): ?>
-            <h1 class="site__name"><?php print $site_name; ?></h1>
+            <a href="<?php print $front_page; ?>" title="<?php print t('Go to home page'); ?>" rel="home">
+              <h1 class="site__name"><?php print $site_name; ?></h1>
+            </a>
           <?php endif; ?>
 
           <?php if ($site_slogan): ?>
